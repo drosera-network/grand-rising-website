011739bb4999072646327e233a87f02c6f5c627a
f1lander <filanderuclez@gmail.com>
2025-05-02 09:16:11 -0600

    tests the deploy astro to gh pages, fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://drosera-network.github.io',
-  base: '/grand-rising-site',
+  base: '/grand-rising-website',
   vite: {
     plugins: [tailwindcss()]
   }
