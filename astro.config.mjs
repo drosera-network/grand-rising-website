9342144be802f3a1890000470921a62ab086d66f
f1lander <filanderuclez@gmail.com>
2025-05-02 10:00:12 -0600

    removes slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://drosera-network.github.io',
-  base: '/grand-rising-website',
+  base: 'grand-rising-website',
   vite: {
     plugins: [tailwindcss()]
   }
